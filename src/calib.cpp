41aa58803f1848b4352842d5ec5b281b95476d65
Aaron-PC\Aaron <pant333@163.com>
2017-01-23 15:29:53 +0800

    change the file structure && simplify the original opencv_calib_example

--- src/calib.cpp
+++ src/calib.cpp
@@ -6,15 +6,7 @@
   
 using namespace std;  
 using namespace cv;  
-  
-const int imageWidth = 640;                             //摄像头的分辨率  
-const int imageHeight = 480;  
-//const int boardWidth = 11;                               //横向的角点数目  
-//const int boardHeight = 7;                              //纵向的角点数据  
-//const int boardCorner = boardWidth * boardHeight;       //总的角点数据  
-//const int frameNumber = 5;                             //相机标定时需要采用的图像帧数  
-//const int squareSize = 30;                              //标定板黑白格子的大小 单位mm  
-//const Size boardSize = Size(boardWidth, boardHeight);   //  
+ 
       
 Mat intrinsic;                                          //相机内参数  
 Mat distortion_coeff;                                   //相机畸变参数  
@@ -22,11 +14,7 @@ vector<Mat> rvecs;                                        //旋转向量
 vector<Mat> tvecs;                                        //平移向量  
 vector<vector<Point2f>> corners;                        //各个图像找到的角点的集合 和objRealPoint 一一对应  
 vector<vector<Point3f>> objRealPoint;                   //各副图像的角点的实际物理坐标集合  
-  
-  
 vector<Point2f> corner;                                   //某一副图像找到的角点  
-  
-Mat rgbImage, grayImage;  
   
 static void help()
 {
@@ -235,7 +223,9 @@ static inline void write(FileStorage& fs, const String&, const Settings& s)
 	s.write(fs);
 }
 
-
+enum { DETECTION = 0, CAPTURING = 1, CALIBRATED = 2 };
+static bool runCalibrationAndSave(Settings& s, Size imageSize, Mat&  cameraMatrix, Mat& distCoeffs,
+	vector<vector<Point2f> > corners);
 /*计算标定板上模块的实际物理坐标*/  
 void calRealPoint(vector<vector<Point3f>>& obj, int boardwidth,int boardheight, int imgNumber, int squaresize)  
 {  
@@ -288,35 +278,6 @@ void guessCameraParam(void )
     distortion_coeff.at<double>(4, 0) = 0;          //p3  
 }  
   
-void outputCameraParam(Settings &s)  
-{  
-    /*保存数据*/ 
-	FileStorage fs;
-	fs.open(s.outputFileName, FileStorage::WRITE);
-    //cvSave("cameraMatrix.xml", &intrinsic);  
-    //cvSave("cameraDistoration.xml", &distortion_coeff);  
-    //cvSave("rotatoVector.xml", &rvecs);  
-    //cvSave("translationVector.xml", &tvecs);  
-    /*输出数据*/  
-	if (fs.isOpened())
-	{
-		fs << "camera_matrix" << intrinsic;
-		fs << "distortion_coefficients" << distortion_coeff;
-		fs.release();
-	}
-
-	cout << "fx :" << intrinsic.at<double>(0, 0) << endl << "fy :" << intrinsic.at<double>(1, 1) << endl;
-	cout << "cx :" << intrinsic.at<double>(0, 2) << endl << "cy :" << intrinsic.at<double>(1, 2) << endl;
-
-	cout << "k1 :" << distortion_coeff.at<double>(0, 0) << endl;
-	cout << "k2 :" << distortion_coeff.at<double>(1, 0) << endl;
-	cout << "p1 :" << distortion_coeff.at<double>(2, 0) << endl;
-	cout << "p2 :" << distortion_coeff.at<double>(3, 0) << endl;
-	cout << "p3 :" << distortion_coeff.at<double>(4, 0) << endl;
-
-}  
-  
-  
 int main(int argc, char* argv[])  
 {
 	help();
@@ -330,17 +291,11 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 	fs["Settings"] >> s;
-	fs.release();                                         // close Settings file
+	fs.release();                                                     // close Settings file
 
-	const string outCameraDataFilePath = "out_camera_data.xml";
+	const string outCameraDataFilePath = "out_camera_data.xml";       // write config as YAML
 	if (outCameraDataFilePath.size() > 1)
 		s.outputFileName = outCameraDataFilePath;
-
-
-	//! [file_read]
-
-	//FileStorage fout("settings.yml", FileStorage::WRITE); // write config as YAML
-	//fout << "Settings" << s;
 
 	if (!s.goodInput)
 	{
@@ -348,102 +303,218 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-    Mat img;  
-    int goodFrameCount = 0;  
-    namedWindow("chessboard");  
-    cout << "按Q退出 ..." << endl;  
-    while (goodFrameCount < s.nrFrames)  
-    {  
-        char filename[100];  
-        sprintf_s(filename,"x%d.bmp", goodFrameCount + 1);  
-    //  cout << filename << endl;  
-        rgbImage = imread(filename, CV_LOAD_IMAGE_COLOR);  
-        cvtColor(rgbImage, grayImage, CV_BGR2GRAY);  
-        imshow("Camera", grayImage);  
+	//[file_read]
+
+	int mode = s.inputType == Settings::IMAGE_LIST ? CAPTURING : DETECTION;
+	Size imageSize;
+	clock_t prevTimestamp = 0;
+	const Scalar RED(0, 0, 255), GREEN(0, 255, 0);
+
+	for (;;)
+	{
+		Mat view;
+		view = s.nextImage();
+		//-----  If no more image, or got enough, then stop calibration and show result -------------
+		if (mode == CAPTURING && corners.size() >= (size_t)s.nrFrames)
+		{
+			if (runCalibrationAndSave(s, imageSize, intrinsic, distortion_coeff, corners))
+				mode = CALIBRATED;
+			else
+				mode = DETECTION;
+		}
+		if (view.empty())          // If there are no more images stop the loop
+		{
+			// if calibration threshold was not reached yet, calibrate now
+			if (mode != CALIBRATED && !corners.empty())
+				runCalibrationAndSave(s, imageSize, intrinsic, distortion_coeff, corners);
+			break;
+		}
+
+		imageSize = view.size();  // Format input image.
+		if (s.flipVertical)    flip(view, view, 0);
+
 		bool isFind;
 		int chessBoardFlags = CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_NORMALIZE_IMAGE;
 		switch (s.calibrationPattern) // Find feature points on the input format
 		{
 		case Settings::CHESSBOARD:
-			isFind = findChessboardCorners(rgbImage, s.boardSize, corner, chessBoardFlags);
+			isFind = findChessboardCorners(view, s.boardSize, corner, chessBoardFlags);
 			break;
 		case Settings::CIRCLES_GRID:
-			isFind = findCirclesGrid(rgbImage, s.boardSize, corner);
+			isFind = findCirclesGrid(view, s.boardSize, corner);
 			break;
 		case Settings::ASYMMETRIC_CIRCLES_GRID:
-			isFind = findCirclesGrid(rgbImage, s.boardSize, corner, CALIB_CB_ASYMMETRIC_GRID);
+			isFind = findCirclesGrid(view, s.boardSize, corner, CALIB_CB_ASYMMETRIC_GRID);
 			break;
 		default:
 			isFind = false;
 			break;
 		}
 
+		if (isFind)                // If done with success,
+		{
+			// improve the found corners' coordinate accuracy for chessboard
+			if (s.calibrationPattern == Settings::CHESSBOARD)
+			{
+				Mat viewGray;
+				cvtColor(view, viewGray, COLOR_BGR2GRAY);
+				cornerSubPix(viewGray, corner, Size(11, 11),
+					Size(-1, -1), TermCriteria(TermCriteria::EPS + TermCriteria::COUNT, 30, 0.1));
+			}
 
-        //bool isFind = findChessboardCorners(rgbImage, s.boardSize, corner,0);  
-        if (isFind == true) //所有角点都被找到 说明这幅图像是可行的  
-        {  
-            /* 
-            Size(5,5) 搜索窗口的一半大小 
-            Size(-1,-1) 死区的一半尺寸 
-            TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 20, 0.1)迭代终止条件 
-            */  
-            cornerSubPix(grayImage, corner, Size(5,5), Size(-1, -1), TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 20, 0.1));  
-            drawChessboardCorners(rgbImage, s.boardSize, corner, isFind);  
-            imshow("chessboard", rgbImage);  
-            corners.push_back(corner);  
-            //string filename = "res\\image\\calibration";  
-            //filename += goodFrameCount + ".jpg";  
-            //cvSaveImage(filename.c_str(), &IplImage(rgbImage));       //把合格的图片保存起来  
-            goodFrameCount++;  
-            cout << "The image is good" << endl;  
-        }  
-        else  
-        {  
-            cout << "The image is bad please try again" << endl;  
-        }  
-    //  cout << "Press any key to continue..." << endl;  
-    //  waitKey(0);  
-  
-       if (waitKey(10) == 'q')  
-        {  
-            break;  
-        }  
-    //  imshow("chessboard", rgbImage);  
-    }  
-  
-    /* 
-    图像采集完毕 接下来开始摄像头的校正 
-    calibrateCamera() 
-    输入参数 objectPoints  角点的实际物理坐标 
-             imagePoints   角点的图像坐标 
-             imageSize     图像的大小 
-    输出参数 
-             cameraMatrix  相机的内参矩阵 
-             distCoeffs    相机的畸变参数 
-             rvecs         旋转矢量(外参数) 
-             tvecs         平移矢量(外参数） 
-    */  
-      
-    /*设置实际初始参数 根据calibrateCamera来 如果flag = 0 也可以不进行设置*/  
-    guessCameraParam();           
-    cout << "guess successful" << endl;  
-    /*计算实际的校正点的三维坐标*/  
-    calRealPoint(objRealPoint, s.boardSize.width, s.boardSize.height,s.nrFrames, s.squareSize);  
-    cout << "cal real successful" << endl;  
-    /*标定摄像头*/  
-    calibrateCamera(objRealPoint, corners, Size(imageWidth, imageHeight), intrinsic, distortion_coeff, rvecs, tvecs, 0);  
-    cout << "calibration successful" << endl;  
-    /*保存并输出参数*/  
-	outputCameraParam(s);
-    cout << "out successful" << endl;  
-      
-    /*显示畸变校正效果*/  
-    Mat cImage;  
-    undistort(rgbImage, cImage, intrinsic, distortion_coeff);  
-    imshow("Correct Image", cImage);  
-    cout << "Correct Image" << endl;  
-    cout << "Wait for Key" << endl;  
-    waitKey(0);  
-    system("pause");  
+			if (mode == CAPTURING &&  // For camera only take new samples after delay time
+				(!s.inputCapture.isOpened() || clock() - prevTimestamp > s.delay*1e-3*CLOCKS_PER_SEC))
+			{
+				corners.push_back(corner);
+				prevTimestamp = clock();
+				//blinkOutput = s.inputCapture.isOpened();
+			}
+
+			// Draw the corners.
+			drawChessboardCorners(view, s.boardSize, Mat(corner), isFind);
+		}
+		else
+		{
+			cout << "The image is bad please try again" << endl;
+		}
+
+		//----------------------------- Output Text ------------------------------------------------
+		//! [output_text]
+		string msg = (mode == CAPTURING) ? "100/100" :
+			mode == CALIBRATED ? "Calibrated" : "Press 'g' to start";
+		int baseLine = 0;
+		Size textSize = getTextSize(msg, 1, 1, 1, &baseLine);
+		Point textOrigin(view.cols - 2 * textSize.width - 10, view.rows - 2 * baseLine - 10);
+
+		if (mode == CAPTURING)
+		{
+			if (s.showUndistorsed)
+				msg = format("%d/%d Undist", (int)corners.size(), s.nrFrames);
+			else
+				msg = format("%d/%d", (int)corners.size(), s.nrFrames);
+		}
+
+		putText(view, msg, textOrigin, 1, 1, mode == CALIBRATED ? GREEN : RED);
+
+		//------------------------- Video capture  output  undistorted ------------------------------
+		//! [output_undistorted]
+		if (mode == CALIBRATED && s.showUndistorsed)
+		{
+			Mat review;
+			undistort(view, review, intrinsic, distortion_coeff);
+			imshow("Correct Image", review);
+		}
+	}
+   
     return 0;  
 }  
+
+// Print camera parameters to the output file
+static void saveCameraParams(Settings& s, Size& imageSize, Mat& cameraMatrix, Mat& distCoeffs,
+	const vector<Mat>& rvecs, const vector<Mat>& tvecs,
+	 const vector<vector<Point2f> >& imagePoints)
+{
+	FileStorage fs(s.outputFileName, FileStorage::WRITE);
+
+	time_t tm;
+	time(&tm);
+	struct tm *t2 = localtime(&tm);
+	char buf[1024];
+	strftime(buf, sizeof(buf), "%c", t2);
+
+	fs << "calibration_time" << buf;
+
+
+	fs << "image_width" << imageSize.width;
+	fs << "image_height" << imageSize.height;
+	fs << "board_width" << s.boardSize.width;
+	fs << "board_height" << s.boardSize.height;
+	fs << "square_size" << s.squareSize;
+
+	if (s.flag & CALIB_FIX_ASPECT_RATIO)
+		fs << "fix_aspect_ratio" << s.aspectRatio;
+
+	if (s.flag)
+	{
+		if (s.useFisheye)
+		{
+			sprintf(buf, "flags:%s%s%s%s%s%s",
+				s.flag & fisheye::CALIB_FIX_SKEW ? " +fix_skew" : "",
+				s.flag & fisheye::CALIB_FIX_K1 ? " +fix_k1" : "",
+				s.flag & fisheye::CALIB_FIX_K2 ? " +fix_k2" : "",
+				s.flag & fisheye::CALIB_FIX_K3 ? " +fix_k3" : "",
+				s.flag & fisheye::CALIB_FIX_K4 ? " +fix_k4" : "",
+				s.flag & fisheye::CALIB_RECOMPUTE_EXTRINSIC ? " +recompute_extrinsic" : "");
+		}
+		else
+		{
+			sprintf(buf, "flags:%s%s%s%s",
+				s.flag & CALIB_USE_INTRINSIC_GUESS ? " +use_intrinsic_guess" : "",
+				s.flag & CALIB_FIX_ASPECT_RATIO ? " +fix_aspectRatio" : "",
+				s.flag & CALIB_FIX_PRINCIPAL_POINT ? " +fix_principal_point" : "",
+				s.flag & CALIB_ZERO_TANGENT_DIST ? " +zero_tangent_dist" : "");
+		}
+		cvWriteComment(*fs, buf, 0);
+	}
+
+	fs << "flags" << s.flag;
+	fs << "camera_matrix" << cameraMatrix;
+	fs << "distortion_coefficients" << distCoeffs;
+
+
+	if (s.writeExtrinsics && !rvecs.empty() && !tvecs.empty())
+	{
+		CV_Assert(rvecs[0].type() == tvecs[0].type());
+		Mat bigmat((int)rvecs.size(), 6, rvecs[0].type());
+		for (size_t i = 0; i < rvecs.size(); i++)
+		{
+			Mat r = bigmat(Range(int(i), int(i + 1)), Range(0, 3));
+			Mat t = bigmat(Range(int(i), int(i + 1)), Range(3, 6));
+
+			CV_Assert(rvecs[i].rows == 3 && rvecs[i].cols == 1);
+			CV_Assert(tvecs[i].rows == 3 && tvecs[i].cols == 1);
+			//*.t() is MatExpr (not Mat) so we can use assignment operator
+			r = rvecs[i].t();
+			t = tvecs[i].t();
+		}
+		//cvWriteComment( *fs, "a set of 6-tuples (rotation vector + translation vector) for each view", 0 );
+		fs << "extrinsic_parameters" << bigmat;
+	}
+
+	/*if (s.writePoints && !imagePoints.empty())
+	{
+		Mat imagePtMat((int)imagePoints.size(), (int)imagePoints[0].size(), CV_32FC2);
+		for (size_t i = 0; i < imagePoints.size(); i++)
+		{
+			Mat r = imagePtMat.row(int(i)).reshape(2, imagePtMat.cols);
+			Mat imgpti(imagePoints[i]);
+			imgpti.copyTo(r);
+		}
+		fs << "image_points" << imagePtMat;
+	}*/
+}
+
+
+//! [run_and_save]
+static bool runCalibrationAndSave(Settings& s, Size imageSize, Mat& cameraMatrix, Mat& distCoeffs,
+	vector<vector<Point2f> > corners)
+{
+	//设置实际初始参数 根据calibrateCamera来 如果flag = 0 也可以不进行设置
+	guessCameraParam();
+	cout << "guess successful" << endl;
+	//计算实际的校正点的三维坐标
+	calRealPoint(objRealPoint, s.boardSize.width, s.boardSize.height, s.nrFrames, s.squareSize);
+	cout << "cal real successful" << endl;
+
+	double totalAvgErr = 0;
+
+	bool ok = calibrateCamera(objRealPoint, corners, Size(imageSize.width, imageSize.height), intrinsic, distortion_coeff, rvecs, tvecs, 0);
+	cout << (ok ? "Calibration succeeded" : "Calibration failed")
+		<< ". avg re projection error = " << totalAvgErr << endl;
+
+	if (ok)
+		//outputCameraParam(s);
+		saveCameraParams(s, imageSize, cameraMatrix, distCoeffs, rvecs, tvecs, corners);
+	return ok;
+}
+//! [run_and_save]
