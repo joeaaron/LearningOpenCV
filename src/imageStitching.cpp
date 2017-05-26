0a1a7cc95b8207cd9c621670ceaf9453cb6ba192
Aaron-PC\Aaron <pant333@163.com>
2017-05-26 11:20:31 +0800

    Image Quality

--- src/imageStitching.cpp
+++ src/imageStitching.cpp
@@ -15,6 +15,7 @@
 #include <vector>  
 using namespace std;
 using namespace cv;
+
 void stitch(std::vector<cv::Mat> imgs, cv::Mat& resultMat)
 {
 	bool Flag = true;
@@ -26,93 +27,89 @@ void stitch(std::vector<cv::Mat> imgs, cv::Mat& resultMat)
 	}
 }
 
-/*----------------------------
-* 功|能 : 多图融合
-*----------------------------
-* 函数 : MulitBlend
-* 访问 : private
-* 返回 : Mat&
-*
-* 参数y : matinput      [in]     图片输入序列D
-* 参数y : imagesrc      [in]     已经-对准的图片
-* 参数y : matloc1       [in]     第一图匹配位置
-* 参数y : matloc2       [in]     第二图匹配位置
-*/
-Mat MultiBlend(deque<Mat>& input, const Mat& src, deque<Point>& loc1, deque<Point>& loc2)
+//计算原始图像点位在经过矩阵变换后在目标图像上对应位置  
+Point2f getTransformPoint(const Point2f originalPoint, const Mat &transformMaxtri)
 {
-	Mat outImg;
-	src.copyTo(outImg);
-	int ioffsetdetail = 0;
-	double dblend = 0.0;
-	for (int i = 0; i < loc1.size() - 1;i++)
-	{
-		int ioffset = loc1[i].y - loc2[i].y;      //row的偏移
-		for (int j = 0; j < 100; j++)
-		{
-			outImg.row(ioffsetdetail + ioffset + j) = input[i].row(ioffset + j)*(1 - dblend) + input[i + 1].row(j) * (dblend);
-			dblend = dblend + 0.01;
-		}
-		ioffsetdetail += ioffset;
-	}
-	return outImg;
+	Mat originelP, targetP;
+	originelP = (Mat_<double>(3, 1) << originalPoint.x, originalPoint.y, 1.0);
+	targetP = transformMaxtri*originelP;
+	float x = targetP.at<double>(0, 0) / targetP.at<double>(2, 0);
+	float y = targetP.at<double>(1, 0) / targetP.at<double>(2, 0);
+	return Point2f(x, y);
 }
 
 void stitch2(Mat& srcImage1, Mat& srcImage2, Mat& panorama)
 {
-	// SURF特征点描述
-	Mat srcImage2Warped;
-	// 初始化SURF检测描述子
-	cv::Ptr<Feature2D> surf = xfeatures2d::SURF::create();
+	//灰度图转换  
+	Mat image1, image2;
+	cvtColor(srcImage1, image1, CV_RGB2GRAY);
+	cvtColor(srcImage2, image2, CV_RGB2GRAY);
 
-	// 关键点及特征描述矩阵声明
-	vector<cv::KeyPoint> kPointMat, kPointMat2;;
-	cv::Mat desMat1, desMat2;
+	//提取特征点    
+	cv::Ptr<Feature2D> sift = xfeatures2d::SIFT::create(800);
 
-	surf->detectAndCompute(srcImage1, Mat(), kPointMat, desMat1);
-	surf->detectAndCompute(srcImage2, Mat(), kPointMat2, desMat2);
+	//SiftFeatureDetector siftDetector(800);  // 海塞矩阵阈值  
+	vector<KeyPoint> keyPoint1, keyPoint2;
+	sift->detect(image1, keyPoint1);
+	sift->detect(image2, keyPoint2);
 
-	// FLANN 关键点匹配
-	// FLANN 初始化 RANSAC一致性 最近邻特征
+	//特征点描述，为下边的特征点匹配做准备    
+	Mat imageDesc1, imageDesc2;
+	sift->compute(image1, keyPoint1, imageDesc1);
+	sift->compute(image2, keyPoint2, imageDesc2);
+
+	//获得匹配特征点，并提取最优配对     
 	FlannBasedMatcher matcher;
-	std::vector< DMatch > matches;
-	matcher.match(desMat1, desMat2, matches);
-	double max_dist = 0, min_dist = 100;
-	// 特征点最大与最小距离查找
-	for (int i = 0; i < desMat1.rows; i++)
+	vector<DMatch> matchePoints;
+	matcher.match(imageDesc1, imageDesc2, matchePoints, Mat());
+	sort(matchePoints.begin(), matchePoints.end()); //特征点排序    
+	//获取排在前N个的最优匹配特征点  
+	vector<Point2f> imagePoints1, imagePoints2;
+	for (int i = 0; i < 10; i++)
 	{
-		double dist = matches[i].distance;
-		if (dist < min_dist) min_dist = dist;
-		if (dist > max_dist) max_dist = dist;
+		imagePoints1.push_back(keyPoint1[matchePoints[i].queryIdx].pt);
+		imagePoints2.push_back(keyPoint2[matchePoints[i].trainIdx].pt);
 	}
-	// 使用good 特征构建距离限定 
-	std::vector< DMatch > good_matches;
-	for (int i = 0; i < desMat1.rows; i++)
+	//获取图像1到图像2的投影映射矩阵，尺寸为3*3  
+	Mat homo = findHomography(imagePoints1, imagePoints2, CV_RANSAC);
+	Mat adjustMat = (Mat_<double>(3, 3) << 1.0, 0, srcImage1.cols, 0, 1.0, 0, 0, 0, 1.0);
+	Mat adjustHomo = adjustMat*homo;
+
+	//获取最强配对点在原始图像和矩阵变换后图像上的对应位置，用于图像拼接点的定位  
+	Point2f originalLinkPoint, targetLinkPoint, basedImagePoint;
+	originalLinkPoint = keyPoint1[matchePoints[0].queryIdx].pt;
+	targetLinkPoint = getTransformPoint(originalLinkPoint, adjustHomo);
+	basedImagePoint = keyPoint2[matchePoints[0].trainIdx].pt;
+
+	//图像配准  
+	Mat imageTransform1;
+	warpPerspective(srcImage1, imageTransform1, adjustMat*homo, Size(srcImage2.cols + srcImage1.cols + 110, srcImage2.rows));
+
+	//在最强匹配点左侧的重叠区域进行累加，是衔接稳定过渡，消除突变  
+	Mat image1Overlap, image2Overlap; //图1和图2的重叠部分     
+	image1Overlap = imageTransform1(Rect(Point(targetLinkPoint.x - basedImagePoint.x, 0), Point(targetLinkPoint.x, srcImage2.rows)));
+	image2Overlap = srcImage2(Rect(0, 0, image1Overlap.cols, image1Overlap.rows));
+	Mat image1ROICopy = image1Overlap.clone();  //复制一份图1的重叠部分  
+	for (int i = 0; i < image1Overlap.rows; i++)
 	{
-		if (matches[i].distance < 3 * min_dist)
+		for (int j = 0; j < image1Overlap.cols; j++)
 		{
-			good_matches.push_back(matches[i]);
+			double weight;
+			weight = (double)j / image1Overlap.cols;  //随距离改变而改变的叠加系数  
+			image1Overlap.at<Vec3b>(i, j)[0] = (1 - weight)*image1ROICopy.at<Vec3b>(i, j)[0] + weight*image2Overlap.at<Vec3b>(i, j)[0];
+			image1Overlap.at<Vec3b>(i, j)[1] = (1 - weight)*image1ROICopy.at<Vec3b>(i, j)[1] + weight*image2Overlap.at<Vec3b>(i, j)[1];
+			image1Overlap.at<Vec3b>(i, j)[2] = (1 - weight)*image1ROICopy.at<Vec3b>(i, j)[2] + weight*image2Overlap.at<Vec3b>(i, j)[2];
 		}
 	}
-	// 图像的关键点匹配
-	std::vector<Point2f> srcImage1_matchedKPs;
-	std::vector<Point2f> srcImage2_matchedKPs;
-	for (size_t i = 0; i < good_matches.size(); i++)
-	{
-		srcImage1_matchedKPs.push_back(
-			kPointMat[good_matches[i].queryIdx].pt);		srcImage2_matchedKPs.push_back(
-			kPointMat2[good_matches[i].trainIdx].pt);
-	}
-	// 计算图像1与图像2的映射
-	Mat H = findHomography(Mat(srcImage2_matchedKPs),
-		Mat(srcImage1_matchedKPs), CV_RANSAC);
-	// 仿射变换
-	warpPerspective(srcImage2, srcImage2Warped, H,
-		Size(srcImage2.cols * 2, srcImage2.rows), INTER_CUBIC);
-	panorama = srcImage2Warped.clone();
-	// 结果输出
-	Mat roi(panorama, Rect(0, 0,
-		srcImage1.cols, srcImage1.rows));
-	srcImage1.copyTo(roi);
+	Mat ROIMat = srcImage2(Rect(Point(image1Overlap.cols, 0), Point(srcImage2.cols, srcImage2.rows)));   //图2中不重合的部分  
+	ROIMat.copyTo(Mat(imageTransform1, Rect(targetLinkPoint.x, 0, ROIMat.cols, srcImage2.rows))); //不重合的部分直接衔接上去  
+	imwrite("./拼接结果.jpg", imageTransform1);
+	
+	panorama = imageTransform1.clone();
+	//// 结果输出
+	//Mat roi(panorama, Rect(0, 0,
+	//	srcImage1.cols, srcImage1.rows));
+	//srcImage1.copyTo(roi);
 }
 int main(int argc, char* argv[])
 {
@@ -125,11 +122,13 @@ int main(int argc, char* argv[])
 	imgs.push_back(image1);
 	imgs.push_back(image2);
 	imgs.push_back(image3);
-	cv::Mat resultMat1, resultMat2;
-	stitch(imgs, resultMat1);
-	cv::imshow("resultMat1", resultMat1);
-	stitch2(image1, image2, resultMat2);
+	cv::Mat resultMat1, resultMat2, resultMat;
+	//stitch(imgs, resultMat1);
+	//cv::imshow("resultMat1", resultMat1);
+	stitch2(image1, image2, resultMat2); 
 	cv::imshow("resultMat2", resultMat2);
+	//stitch2(resultMat2, image3, resultMat);
+	//cv::imshow("resultMat2", resultMat);
 	cv::waitKey(0);
 	return 0;
 }
